e65f16532d100d14463590c29f9f791bbdd38856
Thanh Ngo <nokotavi@gmail.com>
2025-10-07 02:37:50 -0700

    new rules_gleam release prep

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -5,41 +5,6 @@ set -o errexit -o nounset -o pipefail
 # Argument provided by reusable workflow caller, see
 # https://github.com/bazel-contrib/.github/blob/d197a6427c5435ac22e56e33340dff912bc9334e/.github/workflows/release_ruleset.yaml#L72
 TAG=$1
-# The prefix is chosen to match what GitHub generates for source archives
-PREFIX="rules_gleam-${TAG:1}"
-ARCHIVE="rules_gleam-$TAG.tar.gz"
-ARCHIVE_TMP=$(mktemp)
-
-# NB: configuration for 'git archive' is in /.gitattributes
-git archive --format=tar --prefix=${PREFIX}/ ${TAG} >$ARCHIVE_TMP
-
-############
-# Patch up the archive to have integrity hashes for built binaries that we downloaded in the GHA workflow.
-# Now that we've run `git archive` we are free to pollute the working directory.
-
-# Delete the placeholder file
-tar --file $ARCHIVE_TMP --delete ${PREFIX}/tools/integrity.bzl
-
-mkdir -p ${PREFIX}/tools
-cat >${PREFIX}/tools/integrity.bzl <<EOF
-"Generated during release by release_prep.sh"
-
-RELEASED_BINARY_INTEGRITY = $(
-  jq \
-    --from-file .github/workflows/integrity.jq \
-    --slurp \
-    --raw-input go-binaries/*.sha256
-)
-EOF
-
-# Append that generated file back into the archive
-tar --file $ARCHIVE_TMP --append ${PREFIX}/tools/integrity.bzl
-
-# END patch up the archive
-############
-
-gzip <$ARCHIVE_TMP >$ARCHIVE
-SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
 ## Using Bzlmod with Bazel 6
@@ -55,5 +20,3 @@ bazel_dep(name = "rules_gleam", version = "${TAG:1}")
 \`\`\`
 
 EOF
-
-echo "\`\`\`" 
