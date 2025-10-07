6f93a1457e6011a0891a9fe3573c39de2cbf2232
Thanh Ngo <nokotavi@gmail.com>
2025-10-07 02:57:56 -0700

    release archive

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -5,6 +5,11 @@ set -o errexit -o nounset -o pipefail
 # Argument provided by reusable workflow caller, see
 # https://github.com/bazel-contrib/.github/blob/d197a6427c5435ac22e56e33340dff912bc9334e/.github/workflows/release_ruleset.yaml#L72
 TAG=$1
+PREFIX="rules_lint-${TAG:1}"
+ARCHIVE="rules_lint-$TAG.tar.gz"
+
+# NB: configuration for 'git archive' is in /.gitattributes
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} >$ARCHIVE
 
 cat << EOF
 ## Using Bzlmod with Bazel 6
